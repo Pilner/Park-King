1e12c977b44d1ae4d1f772e17ec527dd1dab81be
Pilner <raileyvictuelles@gmail.com>
2024-02-06 07:40:07 +0800

    Added Button Component
    - Button component with two arguments
        - text: string
        - url: string
    - Styled in `globals.css`

---
+++ frontend/src/_components/Button.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+interface ButtonProps {
+	text: string;
+	url: string;
+}
+
+export default function Button({text, url}: ButtonProps) {
+
+  return (
+	<div className={`button-div`}>
+		<Link href={url} scroll={true}>
+		<button className={`round-button`}>{text}</button>
+		</Link>
+	</div>
+  )
+}
