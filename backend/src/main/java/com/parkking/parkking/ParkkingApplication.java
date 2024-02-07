69337afb672274feb65b81380469b02a146318ca
Pilner <raileyvictuelles@gmail.com>
2024-02-08 01:05:04 +0800

    Add(backend/): Initial Java Springboot Backend

---
+++ backend/src/main/java/com/parkking/parkking/ParkkingApplication.java
@@ -0,0 +1,13 @@
+package com.parkking.parkking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkkingApplication.class, args);
+	}
+
+}
